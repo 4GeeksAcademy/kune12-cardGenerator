55696a6f74abec937401550e066b4b2b4e4191f2
Kuyne12 <124099764+Kuyne12@users.noreply.github.com>
2023-06-30 14:24:27 +0000

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  //write you
   console.log("Hello Rigo from the console!");
+  document.querySelector(".card").innerHTML = genrannumber();
+  document.querySelector(".card").classList.add([genransuites()]);
+};
+const genrannumber = () => {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let ranindex = Math.floor(Math.random() * numbers.length);
+  return numbers[ranindex];
+};
+const genransuites = () => {
+  let suites = ["spade", "club", "diam", "heart"];
+  let ranindex = Math.floor(Math.random() * suites.length);
+  return suites[ranindex];
 };
